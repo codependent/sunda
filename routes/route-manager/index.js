ff1d373703ee9ad22b08658135dd191a3d696e13
codependent <joseantonio.inigo@gmail.com>
2014-09-12 14:03:58 +0200

    commit7

--- routes/route-manager/index.js
+++ routes/route-manager/index.js
@@ -3,36 +3,14 @@ var router = express.Router();
 
 module.exports = function(rootRouter, db){
 	router.route('/')
-	
 		.get(function(req, res, next) {
 			db.routes.find({}, function (err, docs) {
 				if(err) throw new Exception(err);
 				res.json(docs);
 			});
-
 			
 		}).post(function(req, res, next){
-			if(req.body.method == 'GET'){
-				rootRouter.route(req.body.path)
-					.get(function(req, res) {
-						res.send('GET called!!!');
-					});
-			}else if(req.body.method == 'POST'){
-				rootRouter.route(req.body.path)
-				.post(function(req, res) {
-					res.send('POST called!!!');
-				});
-			}else if(req.body.method == 'PUT'){
-				rootRouter.route(req.body.path)
-				.put(function(req, res) {
-					res.send('PUT called!!!');
-				});
-			}else if(req.body.method == 'DELETE'){
-				rootRouter.route(req.body.path)
-				.delete(function(req, res) {
-					res.send('DELETE called!!!');
-				});
-			}
+			createExpressRoute(req, res);
 			db.routes.insert({path: req.body.path, method: req.body.method}, function (err, newDoc) {
 				if(err) throw new Exception(err);
   				return res.json(newDoc);
@@ -48,27 +26,58 @@ module.exports = function(rootRouter, db){
 			});
 		});
 	
+
 	router.route('/:id')
-	
 		.delete(function(req, res, next){
-			//TODO Obtener el path del id
-			var path = "/mobmgr/action/devices";
-			var removed = false;
-			for(var i=0; !removed && i< rootRouter.stack.length; i++){
-				if(rootRouter.stack[i].regexp.test(path)){
-					rootRouter.stack.splice(i, 1);
-					console.log(rootRouter.stack);
-					removed=true;
-				}
-			}
-			console.log("about to remove route");
-			db.routes.remove({ _id: req.param('id') }, {}, function (err, numRemoved) {
-				if(err) throw new Exception(err);
-				console.log("number of removed: "+numRemoved);
-  				res.send(200);	
-			});
-			
+			db.routes.findOne({_id : req.param('id')}, function(err, doc){
+				if(err) throw new Exception(err);	
+					console.log("FOUNDDDDD")
+					console.log(doc);			
+					removeExpressRoute(doc.path);
+					db.routes.remove({ _id: req.param('id') }, {}, function (err, numRemoved) {
+					if(err) throw new Exception(err);
+					console.log("number of removed: "+numRemoved);
+	  				res.send(200);	
+				});
+			});			
 		});
+
+
+	function createExpressRoute(req, res){
+		if(req.body.method == 'GET'){
+			rootRouter.route(req.body.path)
+				.get(function(req, res) {
+					res.send('GET called!!!');
+				});
+		}else if(req.body.method == 'POST'){
+			rootRouter.route(req.body.path)
+			.post(function(req, res) {
+				res.send('POST called!!!');
+			});
+		}else if(req.body.method == 'PUT'){
+			rootRouter.route(req.body.path)
+			.put(function(req, res) {
+				res.send('PUT called!!!');
+			});
+		}else if(req.body.method == 'DELETE'){
+			rootRouter.route(req.body.path)
+			.delete(function(req, res) {
+				res.send('DELETE called!!!');
+			});
+		}
+	}
+
+
+	function removeExpressRoute(path){
+		var removed = false;
+		for(var i=0; !removed && i< rootRouter.stack.length; i++){
+			if(rootRouter.stack[i].regexp.test(path)){
+				rootRouter.stack.splice(i, 1);
+				console.log(rootRouter.stack);
+				removed=true;
+			}
+		}
+	}
 
 	return router;
 }
