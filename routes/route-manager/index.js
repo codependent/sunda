84f831cf9f4458955f0f15d965f9ef06f4466b46
codependent <joseantonio.inigo@gmail.com>
2014-09-15 12:35:01 +0200

    commit10

--- routes/route-manager/index.js
+++ routes/route-manager/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(rootRouter, db){
+
+	router.use(function(req, res, next) {
+		if(req.body.path){
+			if(req.body.path.indexOf("/")!=0){
+				req.body.path = "/"+req.body.path;
+			}
+		}
+		next();	
+	});
+
 	router.route('/')
 		.get(function(req, res, next) {
 			db.routes.find({}, function (err, docs) {
@@ -11,15 +21,13 @@ module.exports = function(rootRouter, db){
 			
 		}).post(function(req, res, next){
 			createExpressRoute(req, res);
-			db.routes.insert({path: req.body.path, method: req.body.method}, function (err, newDoc) {
+			db.routes.insert(req.body, function (err, newDoc) {
 				if(err) throw new Exception(err);
   				return res.json(newDoc);
 			});
 						
 		}).put(function(req, res, next){
-			console.log(req.body);
-			console.log(rootRouter);
-			db.routes.update({_id: req.body._id}, { $set: { path: req.body.path, method: req.body.method } }, function (err, numReplaced) {
+			db.routes.update({_id: req.body._id}, req.body , function (err, numReplaced) {
 				if(err) throw new Exception(err);
 				console.log("num updated "+ numReplaced);
 				return res.send(200);
@@ -44,6 +52,7 @@ module.exports = function(rootRouter, db){
 
 
 	function createExpressRoute(req, res){
+		console.log(rootRouter.stack);
 		if(req.body.method == 'GET'){
 			rootRouter.route(req.body.path)
 				.get(function(req, res) {
